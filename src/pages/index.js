baece596b13f02064e67c645bb52f3702911c12f
Navdeep Singh <navdeep.er@gmail.com>
2018-08-29 11:33:17 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi there,</h1>
+    <p>Myself Navdeep Singh hails from India as a Web App Specialist who concerns about UI/UX minutely and understands importance of fast page load for First Paint. I am on always on learning mode be its reading books, online posts, articles, Watching Videos Tutorials etc. </p>
+    <p>This will be my first Online JAMStack Hackathon. So I am focused and hydrated. 🤗</p>
+    <p>My Linkedin Profile: <a href="https://www.linkedin.com/in/navdeep-singh-78639b5/" target="_blank">https://www.linkedin.com/in/navdeep-singh-78639b5/</a></p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
